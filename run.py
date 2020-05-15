7335bfc8035711e1cbdb23bb8e0055d3b1563d90
Michael Scheriger <michael.scheriger@gmail.com>
2020-05-15 10:11:34 -0500

    Added argparsers for pushing to s3 and rds

--- run.py
+++ run.py
@@ -10,7 +10,8 @@ logger = logging.getLogger(__name__)
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description="Create and/or add data to database")
     parser.add_argument('-c', '--create_bucket', action='store_true', default=False, help='If given, a new bucket will be created.')
-
+    parser.add_argument('-p', '--push_to_bucket', action='store_true', default=False, help='If given, data will be pushed to S3 bucket.')
+    parser.add_argument('-r', '--rds_schema', action='store_true', default=False, help='If given, schema for database will be created.'
     args = parser.parse_args()
 
     ###READ CONFIG FILE
@@ -26,7 +27,9 @@ if __name__=='__main__':
     ###CREATE BUCKET AND PUSH DATABASE TO S3
     if args.create_bucket:
         source_bucket(config_s3['bucket_name'],config_s3['location'])
-    push_data(config_s3['data_path'],config_s3['bucket_name'],config_s3['database_name'])
+    if args.push_to_bucket:
+        push_data(config_s3['data_path'],config_s3['bucket_name'],config_s3['database_name'])
 
     ###Create RDS Schema
-    create_table(local=config_rds['local'],local_location=config_rds['db_path'])
+    if args.rds_schema:
+        create_table(local=config_rds['local'],local_location=config_rds['db_path'])
